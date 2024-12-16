80eb31ae21b56ff1f90b97cd011b3c9edb23ea87
lucashsu95 <lucashsu95@gmail.com>
2024-12-16 17:23:44 +0800

    Refactor path processing and update ignore list in DFS function

--- src/create_path/main.py
+++ src/create_path/main.py
@@ -3,11 +3,11 @@
 import os
 
 def process_path(name:str,directory:str):
-    return '/'.join(name.replace(directory,'').replace(' ', '%20').split('\\'))[1:]
+    return '/'.join(name.replace(directory, '').replace(' ', '%20').split('\\'))[1:]
 
 def dfs(directory, output, folder_indexes,depth=1,maxDepth=True):
     try:
-        ignore_list = ('.git', 'README.md', 'path.md', 'folder_path_readme_generator.py')
+        ignore_list = ('.git', 'README.md', 'path.md')
         items = list(filter(lambda x: x not in ignore_list, os.listdir(directory)))
         items = sorted(items,key=lambda x: (os.path.isdir(os.path.join(directory, x))),reverse=True)
         folder_index = 0
@@ -18,17 +18,18 @@ def dfs(directory, output, folder_indexes,depth=1,maxDepth=True):
 
             if os.path.isdir(item_path):
                 folder_indexes[-1] = str(int(folder_indexes[-1]) + 1)
-                print(f"{space}- {'#' * min(depth, 6)} 第{'-'.join(folder_indexes)}章 [{item}]({process_path(item_path,directory)})",file=output)
+                print(f"{space}- {'#' * min(depth, 6)} 第{'-'.join(folder_indexes)}章 [{item}]({process_path(item_path,folder_name)})",file=output)
                 if maxDepth == True or depth < maxDepth:
                     dfs(item_path, output,folder_indexes + [str(folder_index)], depth + 1,maxDepth)
             else:
                 title = f"{'-'.join(folder_indexes[:-1])}_**{idx:02d}**"
-                print(f"{space}- {title} [{item}]({process_path(item_path,directory)})",file=output)
+                print(f"{space}- {title} [{item}]({process_path(item_path,folder_name)})",file=output)
 
     except OSError:
         print(f"Error reading files in directory: {directory}")
 
 def main(path="."):
+    global folder_name
     folder_name = os.path.abspath(path)
     os.chdir(folder_name)
     output_file_path = os.path.join(folder_name, 'path.md')
